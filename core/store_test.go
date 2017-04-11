d12c69baf9a2bd8644603f6f44c7396c4a811ee0
hmli <lihm@welomo.com>
2017-04-12 00:22:58 +0800

    fragment tested

--- core/store_test.go
+++ core/store_test.go
@@ -28,14 +28,26 @@ func TestExt(t *testing.T) {
 	t.Log(Ext(f3))
 	f4 := "ddd.fff.ggg.hhh"
 	t.Log(Ext(f4))
+	f5 := "ttt"
+	t.Log(Ext(f5))
+	f6 := "."
+	t.Log(Ext(f6))
+	f7 := "..."
+	t.Log(Ext(f7))
 }
 
 func TestVolume_Fragment(t *testing.T) {
 	v, err := NewVolume(1, "/tmp/fs")
 	id, err := v.NewFile([]byte("fff"), "f0")
 	assert.NoError(t, err)
+	id2 , err := v.NewFile([]byte("ddd"), "d1")
+	assert.NoError(t, err)
+	id3, err := v.NewFile([]byte("sss"), "s2")
+	assert.NoError(t, err)
+	id4, err := v.NewFile([]byte("aaa"), "a3")
+	assert.NoError(t, err)
+	t.Log(id, id2, id3, id4)
 	v.Print()
-	t.Log(id)
 	v.DelNeedle(id)
 	v.Print()
 	fl, _ := v.File.Stat()
@@ -46,10 +58,9 @@ func TestVolume_Fragment(t *testing.T) {
 	}
 	assert.NoError(t, err)
 	v.Print()
-	//has := v.Directory.Has(id)
-	//assert.True(t, has)
 	fl, _ = v.File.Stat()
 	sizeNew := fl.Size()
+	assert.Equal(t, sizeOld-sizeNew, int64(3 + NeedleFixSize))
 	t.Log(sizeOld, sizeNew)
 }
 
@@ -57,7 +68,6 @@ func TestVolume_PrintFile(t *testing.T) {
 	v, err := NewVolume(1, "/tmp/fs")
 	assert.NoError(t, err)
 	//v.NewNeedle(1, []byte("20"), "test.jpg")
-	v.PrintFile()
 	header, err := v.ReadHeader(8)
 	assert.NoError(t, err)
 	t.Log(header, len(header))
--- utils/id_generator.go
+++ utils/id_generator.go
@@ -3,5 +3,5 @@ package utils
 import "time"
 
 func UniqueId() (id uint64) {
-	return uint64(time.Now().Unix())
+	return uint64(time.Now().UnixNano())
 }
